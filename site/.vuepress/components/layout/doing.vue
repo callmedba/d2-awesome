7d192affee715f83da426fe4f180bc582760e217
liyang <1711467488@qq.com>
2018-10-21 11:23:26 +0800

    no message

--- site/.vuepress/components/layout/doing.vue
+++ site/.vuepress/components/layout/doing.vue
@@ -5,6 +5,12 @@
     <img
       class="component_cover"
       :src="$withBase('/resource/page/doing/cover@2x.png')">
+    <p>功能正在开发 ...</p>
+    <a
+      class="button is-rounded"
+      :href="$withBase('/')">
+      返回首页
+    </a>
   </div>
 </template>
 
@@ -13,6 +19,9 @@
   height: 100vh;
   .component_cover {
     width: 200px;
+  }
+  p {
+    margin-bottom: 1em;
   }
 }
 </style>
