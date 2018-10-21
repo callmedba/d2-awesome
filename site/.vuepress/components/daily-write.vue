42ac32d07dff7079124d94f811b6b2ff584ac86f
liyang <1711467488@qq.com>
2018-10-21 17:03:46 +0800

    no message

--- site/.vuepress/components/daily-write.vue
+++ site/.vuepress/components/daily-write.vue
@@ -75,7 +75,7 @@
         <section class="modal-card-body">
           <p>感谢您的分享，但是还有一个步骤需要您来完成：</p>
           <p>由于本站为由 <code>vue</code> + <code>markdown</code> 构建纯静态站点，所以您的分享需要以 issue 方式反馈给我们。</p>
-          <p><b>填写内容已经复制到您的剪贴板</b>，请点击下面的按钮跳转至 Github，将其复制进 issue 内容中。（issue 标题请注明“日报提交”）<b>工作日我们将在一小时内处理</b>。</p>
+          <p><b>填写内容已经复制到您的剪贴板</b>（其实也可以随便写），请点击下面的按钮跳转至 Github，将其复制进 issue 内容中。（issue 标题请注明“日报提交”）<b>工作日我们将在一小时内处理</b>。</p>
           <p>
             <a
               class="button is-success"
