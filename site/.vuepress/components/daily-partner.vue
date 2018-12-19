ede782c2f31f509559ae8010bb3d977c24b2cd8f
liyang <1711467488@qq.com>
2018-12-19 10:57:26 +0800

    合作方添加掘金

--- site/.vuepress/components/daily-partner.vue
+++ site/.vuepress/components/daily-partner.vue
@@ -5,9 +5,17 @@
     </div>
     <div class="card-content">
       <a
+        href="https://juejin.im"
+        target="_blank"
+        class="button is-large daily-partner--button">
+        <img
+          class="daily-partner--logo"
+          :src="$withBase('/resource/component/daily-partner/juejin.png')">
+      </a>
+      <a
         href="https://github.com/521xueweihan"
         target="_blank"
-        class="button is-large">
+        class="button is-large daily-partner--button">
         <img
           class="daily-partner--logo"
           :src="$withBase('/resource/component/daily-partner/hello-github.png')">
@@ -15,7 +23,7 @@
       <a
         href="https://alibaba.github.io/ice"
         target="_blank"
-        class="button is-large">
+        class="button is-large daily-partner--button">
         <img
           class="daily-partner--logo"
           :src="$withBase('/resource/component/daily-partner/ice.png')">
@@ -27,5 +35,9 @@
 <style lang="scss" scoped>
 .daily-partner--logo {
   height: 40px;
+}
+.daily-partner--button {
+  padding-left: .4em;
+  padding-right: .4em;
 }
 </style>
